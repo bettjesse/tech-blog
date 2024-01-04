8e2b261797a0ab8b1a02dbbae7c5cdab5d06770e
jesse <jessebett1999@gmail.com>
2024-01-04 14:12:01 +0300

    react-hot-toast package

--- actions/getBlogs.ts
+++ actions/getBlogs.ts
@@ -1,4 +1,4 @@
-// 
+
 import { Category, Blog } from "@prisma/client";
 import { db } from "@/lib/db";
 
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -26,19 +26,24 @@ const page = async({searchParams}: SearchPageProps) => {
   })
   
   const blogs = await getBlogs({...searchParams})
-  console.log("[BLOGS]", blogs)
+  // console.log("[BLOGS]", blogs)
   return (
     <>
     <div className="px-6 pt-6 md:hidden md:mb-0 block">
       <SearchInput/>
 
     </div>
-    <div className="p-6"> 
+    <div className="p-6 space-y-4"> 
        <Categories
        items = {categories}
        
        />
-       <BlogsList items ={blogs} />
+
+       <BlogsList 
+       items ={blogs}
+      
+       
+       />
     </div>
     </>
   )
